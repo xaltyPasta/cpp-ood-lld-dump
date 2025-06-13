c6d73c14dcb88cba7b240935943d2c7367f191a5
xaltyPasta <rajkumarravi9470@gmail.com>
2025-06-13 12:50:32 +0530

    ms-excel lld

---
+++ MSExcel.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Cell
+{
+    string value;
+    string formula;
+    vector<Cell *> dependents;
+
+    void setValue(const string &val);
+    void setFormula(const string &f);
+    string evaluate();
+};
+
+class Sheet
+{
+private:
+    int rows, cols;
+    vector<vector<Cell>> Cells;
+
+public:
+    Sheet(int rows, int cols);
+    Cell &getCell(int row, int col);
+    Cell &getCellByRef(const string &ref);
+    void setCellValue(int row, int col, const string &value);
+    void setCellFormula(int row, int col, const string &formula);
+    void updateDependencies(Cell &cell);
+};
+
+class FormulaParser
+{
+public:
+    static string evaluate(const string &formula, Sheet &sheet);
+};
+
+int main()
+{
+    return 0;
+}
